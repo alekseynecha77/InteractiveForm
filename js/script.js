18370e9d1a0700ac061174844b420c827ec035a6
alekseynecha77 <68791158+alekseynecha77@users.noreply.github.com>
2022-10-12 13:39:10 -0400

    Update script.js

--- js/script.js
+++ js/script.js
@@ -2,6 +2,7 @@ const shirtColor = document.getElementById("color");
 const shirtColorOption = document.querySelectorAll("#color option");
 const shirtDesign = document.getElementById("design");
 const e = document.getElementById("title");
+const activities = document.getElementById('activities');
 
 
 window.onload = function() {
@@ -23,52 +24,27 @@ if(value === 'other'){
 
 })
 
-shirtDesign.addEventListener('change', ()=>{
+shirtDesign.addEventListener('click', ()=>{
     shirtColor.disabled = false;
 
 for(var i = 0; i < shirtColor.length; i++){
-
+var dataTheme = shirtColor.getAttribute('data-theme');
     //JS Puns
-    if(shirtColor[i].value === 'tomato'){
-        shirtColor[i].style.display = 'none';
-    }
-    if(shirtColor[i].value === 'steelblue'){
-        shirtColor[i].style.display = 'none';
-    }
-    if(shirtColor[i].value === 'dimgrey'){
-        shirtColor[i].style.display = 'none';
-    }
-    if(shirtColor[i].value === 'cornflowerblue'){
+    if(shirtColor[i].value === dataTheme){
         shirtColor[i].style.display = 'block';
     }
-    if(shirtColor[i].value === 'darkslategrey'){
-        shirtColor[i].style.display = 'block';
-    }
-    if(shirtColor[i].value === 'gold'){
-        shirtColor[i].style.display = 'block';
-    }
+    else{
+        shirtColor[i].style.display = 'none';
 
-    // I ♥ JS
-
-    if(shirtColor[i].value === 'tomato'){
-        shirtColor[i].style.display = 'block';
-    }
-    if(shirtColor[i].value === 'steelblue'){
-        shirtColor[i].style.display = 'block';
-    }
-    if(shirtColor[i].value === 'dimgrey'){
-        shirtColor[i].style.display = 'block';
-    }
-    if(shirtColor[i].value === 'cornflowerblue'){
-        shirtColor[i].style.display = 'none';
-    }
-    if(shirtColor[i].value === 'darkslategrey'){
-        shirtColor[i].style.display = 'none';
-    }
-    if(shirtColor[i].value === 'gold'){
-        shirtColor[i].style.display = 'none';
     }
 }
 
 
 })
+
+// activities.onchange =(e)=>{
+// const click = e.target;
+// const cost = click.getAttribute('data-cost');
+// const parsedCost = parseInt(cost);
+
+// }
